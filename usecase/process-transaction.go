9e97ce0684cedbd20871b875dedd89a90a2b885c
renato97 <renato.oliveira83@outlook.com>
2022-08-15 19:42:57 -0300

    feat: create Transaction usecase factory

--- usecase/process-transaction.go
+++ usecase/process-transaction.go
@@ -5,3 +5,7 @@ import "github.com/simplemoney/codebank/domain"
 type UseCaseTransaction struct {
 	TransactionRepository domain.TransactionRepository
 }
+
+func NewUseCaseTransaction(transactionRepository domain.TransactionRepository) UseCaseTransaction {
+	return UseCaseTransaction{TransactionRepository: transactionRepository}
+}
