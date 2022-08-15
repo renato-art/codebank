b675746e33d7c20ad62cec7723788898afa1cfb4
renato97 <renato.oliveira83@outlook.com>
2022-08-15 18:36:53 -0300

    feat: create NewTransaction function

--- domain/transaction.go
+++ domain/transaction.go
@@ -1,6 +1,10 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
 
 type Transaction struct {
 	ID           string
@@ -10,4 +14,11 @@ type Transaction struct {
 	Store        string
 	CreditCardId string
 	CreatedAt    time.Time
+}
+
+func NewTransaction() *Transaction {
+	t := &Transaction{}
+	t.ID = uuid.NewV4().String()
+	t.CreatedAt = time.Now()
+	return t
 }
